45ceeee6de26371af2860cb805b034ba9a963c08
Uncanny01 <dhruv.sni.02@gmail.com>
2024-02-15 10:56:20 +0530

    Solution file added

--- 2824.cpp
+++ 2824.cpp
@@ -2,17 +2,17 @@ class Solution {
 public:
     int countPairs(vector<int>& nums, int target) {
         int n=nums.size();
-        int counter=0;
+        int count=0;
         for(int i=0;i<n-1;i++)
         {
             for(int j=i+1; j<n;j++)
             {
                 if(i<j<n && nums[i]+nums[j]<target)
                 {
-                    counter++;
+                    count++;
                 }
             }
         }
-        return counter;
+        return count;
     }
 };
